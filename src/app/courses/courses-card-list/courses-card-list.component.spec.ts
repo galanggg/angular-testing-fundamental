75f216f0c79d0f905a77f1995186a5c6cfe66523
galanggg <tugasgalang@gmail.com>
2022-06-29 20:53:30 +0800

    feat: add test for course card list component

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -16,6 +16,7 @@ import { setupCourses } from "../common/setup-test-data";
 describe("CoursesCardListComponent", () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(
     waitForAsync(() => {
@@ -26,6 +27,7 @@ describe("CoursesCardListComponent", () => {
         .then(() => {
           fixture = TestBed.createComponent(CoursesCardListComponent);
           component = fixture.componentInstance;
+          el = fixture.debugElement;
         });
     })
   );
@@ -35,10 +37,31 @@ describe("CoursesCardListComponent", () => {
   });
 
   it("should display the course list", () => {
-    pending();
+    component.courses = setupCourses();
+
+    fixture.detectChanges();
+
+    const cards = el.queryAll(By.css(".course-card"));
+
+    expect(cards).toBeTruthy("Could not find cards");
+    expect(cards.length).toBe(12, "Unexpected number of courses");
   });
 
   it("should display the first course", () => {
-    pending();
+    component.courses = setupCourses();
+
+    fixture.detectChanges();
+
+    const course = component.courses[0];
+
+    const card = el.query(By.css(".course-card:first-child")),
+      title = card.query(By.css("mat-card-title")),
+      image = card.query(By.css("img"));
+
+    expect(card).toBeTruthy("could not find course card");
+
+    expect(title.nativeElement.textContent).toBe(course.titles.description);
+
+    expect(image.nativeElement.src).toBe(course.iconUrl);
   });
 });
